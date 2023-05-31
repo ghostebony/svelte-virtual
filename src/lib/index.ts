e32fdb42f5639a57b068df6439711ba54da7d210
Pedro Américo <pedroamericolouzada@uol.com.br>
2023-05-31 11:40:16 -0300

    directly export svelte files

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,2 @@
-export { default as Grid } from "./grid";
-export { default as List } from "./list";
+export { default as Grid } from "./grid/Grid.svelte";
+export { default as List } from "./list/List.svelte";
